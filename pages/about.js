332049a71b398f0591aad663b6b0ef68b73bb70b
Daniel Paiva <danielcspaiva@gmail.com>
2020-05-13 11:38:23 -0300

    added mindmap

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function about() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Link href="/"><a>Go Back</a></Link>
+    </div>
+  )
+}
