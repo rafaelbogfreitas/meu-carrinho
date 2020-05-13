c9e4010185bac3af95a8423ec7d18440f3d8a67e
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-12 21:51:42 -0300

    Prepare environment

---
+++ components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import loginStyles from './login.module.scss';
+
+const Login = () => {
+  return (
+    <div className={loginStyles.login}>
+      Login
+    </div>
+  )
+}
+
+export default Login;
---
+++ components/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import signupStyles from './signup.module.scss';
+
+const Signup = () => {
+  return (
+    <div className={signupStyles.login}>
+      Signup
+    </div>
+  )
+}
+
+export default Signup;
