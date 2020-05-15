778b2c2019eda53235babb54668afe25c99b1ebc
Daniel Paiva <danielcspaiva@gmail.com>
2020-05-15 19:44:03 -0300

    minor improvements made to test specs. EditStore spec not yet implemented

--- cypress/integration/createStore.spec.js
+++ cypress/integration/createStore.spec.js
@@ -1,7 +1,7 @@
 describe('Criar Nova Loja', () => {
-  const storeName = 'Loja teste cypress'
-  const storeDescription = 'Descricao teste cypress'
-  const phone = '99999999999'
+  const storeName = 'BOARICKY'
+  const storeDescription = 'Descricao do rafael'
+  const phone = '1111111111111'
   const storePhoto = '../test data/carrinho-compras.png'
 
   beforeEach(() => {
@@ -14,34 +14,46 @@ describe('Criar Nova Loja', () => {
     cy.location('pathname').should('eq', '/createStore')
   })
 
-  it('Preencher form de nova loja', () => {
+  it('Preencher nome da nova loja', () => {
     cy.get('[placeholder="Minha loja"]')
       .type(storeName)
       .should('have.value', storeName)
-
+  })
+  it('Preencher descrição da nova loja', () => {
     cy.get('textarea')
       .type(storeDescription)
       .should('have.value', storeDescription)
-
-    cy.get('[name="primaryColor"]')
-      .invoke('val', '#fe0090')
-      .trigger('change')
-
-    cy.get('[name="secondaryColor"]')
-      .invoke('val', '#ff9999')
-      .trigger('change')
-      
+  })
+  it('Preencher telefone da nova loja', () => {
     cy.get('[placeholder="(xx) xxxxx-xxxx"]')
       .type(phone)
       .should('have.value', phone)
-
+  })
+  it('Selecionar foto da nova loja', () => {
     cy.get('[type="file"]')
       .attachFile(storePhoto);
+  })
+  it('Selecionar cor primária da nova loja', () => {
+    cy.get('[name="primaryColor"]')
+      .invoke('val', '#fe0090')
+      .trigger('change')
+      .should('have.value', '#fe0090')
+  })
+  it('Selecionar cor secundária da nova loja', () => {
+    cy.get('[name="secondaryColor"]')
+      .invoke('val', '#ff9999')
+      .trigger('change')
+      .should('have.value', '#ff9999')
   })
 
   it('Subemeter formulário', () => {
     cy.get('button')
       .click()
+  })
+
+  it('Ir para Nova Loja', () => {
+    cy.get('h1').contains(`${storeName}`).click()
+    cy.location('pathname').should('eq', `/store/${storeName}/dashboard`)
   })
 })
 
---
+++ cypress/integration/editStore.spec.js
@@ -0,0 +1,53 @@
+// describe('Criar Nova Loja', () => {
+//   const storeName = 'xxxxxxxxxx'
+//   const storeDescription = 'xxxxxxxxxx'
+//   const phone = 'xxxxxxxxxxx'
+//   const storePhoto = '../test data/carrinho-compras.png'
+
+//   beforeEach(() => {
+//     Cypress.Cookies.preserveOnce('session_id', 'remember_token', 'connect.sid')
+//   })
+
+//   it('Preencher nome da nova loja', () => {
+//     cy.get('[placeholder="Minha loja"]')
+//       .type(storeName)
+//       .should('have.value', storeName)
+//   })
+//   it('Preencher descrição da nova loja', () => {
+//     cy.get('textarea')
+//       .type(storeDescription)
+//       .should('have.value', storeDescription)
+//   })
+//   it('Preencher telefone da nova loja', () => {
+//     cy.get('[placeholder="(xx) xxxxx-xxxx"]')
+//       .type(phone)
+//       .should('have.value', phone)
+//   })
+//   it('Selecionar foto da nova loja', () => {
+//     cy.get('[type="file"]')
+//       .attachFile(storePhoto);
+//   })
+//   it('Selecionar cor primária da nova loja', () => {
+//     cy.get('[name="primaryColor"]')
+//       .invoke('val', '#fe0090')
+//       .trigger('change')
+//       .should('have.value', '#fe0090')
+//   })
+//   it('Selecionar cor secundária da nova loja', () => {
+//     cy.get('[name="secondaryColor"]')
+//       .invoke('val', '#ff9999')
+//       .trigger('change')
+//       .should('have.value', '#ff9999')
+//   })
+
+//   it('Subemeter formulário', () => {
+//     cy.get('button')
+//       .click()
+//   })
+
+//   it('Ir para Nova Loja', () => {
+//     cy.get('h1').contains(`${storeName}`).click()
+//     cy.location('pathname').should('eq', `/store/${storeName}/dashboard`)
+//   })
+// })
+
