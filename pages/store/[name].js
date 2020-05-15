f41828b3407f3df1048b2997aacfd7a9908adeca
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-15 11:31:05 -0300

    add service to getInitialProps on store page

--- pages/store/[name].js
+++ pages/store/[name].js
@@ -1,41 +1,41 @@
-import React from 'react'
-import Link from 'next/link'
-import axios from 'axios'
+import React from 'react';
+import Link from 'next/link';
+import { getStore } from '../../services/storeServices';
 
-
-export default function Store({ store }){
-  // let router = useRouter();
-
-
+export default function Store(props) {
   return (
     <div>
-      <h1 style={{backgroundColor: `${store.theme.secondaryColor}`}}>Welcome to {store.name} store!!</h1>
+      <h1 style={{ backgroundColor: `${props.theme.secondaryColor}` }}>
+        Welcome to {props.name} store!!
+      </h1>
       <div className="products">
-        {store.products.map( product => {
+        {props.products.map((product) => {
           return (
-            <Link href={'/product/[id]'} as={`/product/${product._id}`} key={product._id}>
+            <Link
+              href={'/product/[id]'}
+              as={`/product/${product._id}`}
+              key={product._id}
+            >
               <a key={product._id}>{product.name}</a>
             </Link>
-          )
-          }
-        )}
-        <Link href={'/store/paineldevendas/[name]'} as={`/store/paineldevendas/${store.name}`}>
+          );
+        })}
+        <Link
+          href={'/store/paineldevendas/[name]'}
+          as={`/store/paineldevendas/${props.name}`}
+        >
           <a>Painel de vendas</a>
         </Link>
-        <Link href={'/editStore/[name]'} as={`/editStore/${store.name}`}>
+        <Link href={'/editStore/[name]'} as={`/editStore/${props.name}`}>
           <a>Editar</a>
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
-
-Store.getInitialProps = async ctx => {
-  let { name } = ctx.query;
-  const store = await axios.get(`http://localhost:5000/api/v1/store/${name}`)
-  return {
-    store: store.data[0]
-  }
-}
-
+Store.getInitialProps = async (context) => {
+  const { name } = context.query;
+  const [store] = await getStore(name);
+  return store;
+};
