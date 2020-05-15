84b552da0e69d1b1ae932c39e92257272d23d038
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-15 13:59:43 -0300

    add helpers to editStore

--- pages/editStore/[name].js
+++ pages/editStore/[name].js
@@ -1,49 +1,32 @@
-import { useState, useEffect } from 'react';
-import { editStore, getStore, deleteStore } from '../../services/storeServices';
+import { useState } from 'react';
+import { editStore, getStore, deleteStore } from '../../services/storeService';
 import ProtectedRoute from '../../components/ProtectedRoute/ProtectedRoute';
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import {
+  handleInputChange,
+  handleFileChange,
+  processFormData,
+} from '../../services/helpers';
 
-
-const Name = (props) => {
+const Name = ({ store }) => {
   const router = useRouter();
-  const [name, setName] = useState(props.name);
-  const [about, setAbout] = useState(props.about);
-  const [primaryColor, setPrimaryColor] = useState(props.theme.primaryColor);
-  const [secondaryColor, setSecondaryColor] = useState(props.theme.seconfaryor);
-  const [phone, setPhone] = useState(props.phone);
-  const [file, setFile] = useState(false);
 
-  const handleInputName = (event) => {
-    setName(event.target.value);
-  };
+  const [name, setName] = useState(store.name);
+  const [about, setAbout] = useState(store.about);
+  const [primaryColor, setPrimaryColor] = useState(store.theme.primaryColor);
+  const [secondaryColor, setSecondaryColor] = useState(store.theme.secondaryColor);
+  const [phone, setPhone] = useState(store.phone);
+  const [image, setImage] = useState(false);
 
-  const handleInputAbout = (event) => {
-    setAbout(event.target.value);
-  };
-
-  const handleInputPrimaryColor = (event) => {
-    setPrimaryColor(event.target.value);
-  };
-
-  const handleInputSecondaryColor = (event) => {
-    setSecondaryColor(event.target.value);
-  };
-
-  const handleInputNumber = (event) => {
-    setPhone(event.target.value);
-  };
-
-  const handleInputFile = (e) => {
-    const uploadData = new FormData();
-    uploadData.append('image', e.target.files[0]);
-    setFile(uploadData);
-  };
+  const { _id: id } = store;
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const body = { name, about, primaryColor, secondaryColor, phone, file };
-    editStore(props._id, body)
+
+    const data = { name, about, primaryColor, secondaryColor, phone, image };
+
+    editStore(id, processFormData(data))
       .then((response) => {
         console.log('>>> response: ', response);
         router.push(`/store/${response.updatedStore.name}`);
@@ -54,7 +37,7 @@ const Name = (props) => {
   };
 
   const handleDelete = () => {
-    deleteStore(props._id)
+    deleteStore(id)
       .then((response) => {
         console.log('response: ', response);
         router.push('/minhaslojas');
@@ -73,7 +56,7 @@ const Name = (props) => {
           placeholder="Minha loja"
           name="name"
           value={name}
-          onChange={handleInputName}
+          onChange={(event) => handleInputChange(event, setName)}
         />
         <label htmlFor="about">Sobre sua loja:</label>
         <textarea
@@ -81,21 +64,21 @@ const Name = (props) => {
           placeholder="Uma loja sobre..."
           name="about"
           value={about}
-          onChange={handleInputAbout}
+          onChange={(event) => handleInputChange(event, setAbout)}
         />
         <label htmlFor="primaryColor">Cor primária:</label>
         <input
           type="color"
           name="primaryColor"
           value={primaryColor}
-          onChange={handleInputPrimaryColor}
+          onChange={(event) => handleInputChange(event, setPrimaryColor)}
         />
         <label htmlFor="secondaryColor">Cor secundária:</label>
         <input
           type="color"
           name="secondaryColor"
           value={secondaryColor}
-          onChange={handleInputSecondaryColor}
+          onChange={(event) => handleInputChange(event, setSecondaryColor)}
         />
         <label htmlFor="phone">Telefone</label>
         <input
@@ -103,22 +86,24 @@ const Name = (props) => {
           placeholder="(xx) xxxxx-xxxx"
           name="phone"
           value={phone}
-          onChange={handleInputNumber}
+          onChange={(event) => handleInputChange(event, setPhone)}
         />
         <label htmlFor="image">Sua logo:</label>
-        <input type="file" name="image" onChange={handleInputFile} />
+        <input type="file" name="image" onChange={(event) => handleFileChange(event, setImage)} />
         <button type="submit">Salvar edições</button>
       </form>
       <button onClick={handleDelete}>Apagar loja</button>
-      <Link href="minhaslojas"><a>Ir para minhas lojas</a></Link>
+      <Link href="minhaslojas">
+        <a>Ir para minhas lojas</a>
+      </Link>
     </ProtectedRoute>
-  )
+  );
 };
 
 Name.getInitialProps = async (context) => {
   const { name } = context.query;
-  const response = await getStore(name);
-  return { ...response[0] };
+  const [store] = await getStore(name);
+  return { store };
 };
 
 export default Name;
