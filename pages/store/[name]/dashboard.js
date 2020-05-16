a8247137d1ebc02647fed7e75a7879ce7039ad6e
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-15 22:54:43 -0300

    Add logics to shopping cart

--- pages/store/[name]/dashboard.js
+++ pages/store/[name]/dashboard.js
@@ -2,55 +2,89 @@ import React, { useState, useEffect } from 'react';
 
 import Head from 'next/head'
 import Link from 'next/link';
-import Router from 'next/router';
 import { getStore } from '../../../services/storeService';
 
 
 export default function Store({store}) {
-
+  
   let [ products, setProducts ] = useState(store.products)
   let [ cart, setCart ] = useState([]);
+
 
   useEffect(() => {
     window.localStorage.setItem('products', JSON.stringify(products))
   }, [products])
 
   const handleProduct = id => {
+    let newProduct = {};
+    let cartProduct = {};
+   
     const updatedProducts = [...products].map(product => {
-      // const productCopy = JSON.parse(JSON.stringify(product));
+
       if (product._id === id) {
-        const newProduct = {
+        
+        newProduct = {
           ...product,
           quantity: product.quantity - 1
         }
-        // handleCart(newProduct);
+
+        cartProduct = {
+          ...product,
+          quantity: 1
+        }
+
+        return newProduct;
       }
       return product;
     });
-    console.log(updatedProducts);
+
+    if(newProduct.quantity < 0) return
+    
     setProducts(updatedProducts);
+
+    //handleCart
+    if(!cart.some(item => item._id == cartProduct._id) || cart.length == 0){
+      setCart([...cart, cartProduct]);
+      return
+    } 
+
+    let updatedCart = [...cart].map( (item, i, arr) => 
+      item._id == cartProduct._id ?
+      ({
+        ...cartProduct,
+        quantity: item.quantity + 1
+      }) :
+      item
+    )
+
+    setCart(updatedCart)
   }
 
-  const handleCart = (product) => {
+  const removeItemsFromCart = id => {
+    let updatedProduct = {}
+    let updatedCart = [...cart].filter( item => {
+      if(item._id != id) {
+        return true
+      } else {
+        updatedProduct = {
+          ...item,
+        }
+        return false;
+      };
+    });
 
-    // if (cart.findIndex(item => item._id === product._id) === -1) {
-    //   console.log('aqui')
-    //   setCart([...cart, product]);
-    //   return;
-    // }
-
-    // const updatedCart = [...cart].map(item => {
-    //   if (item._id === product._id) {
-    //     const newItem = {
-    //       ...item,
-    //       quantity: item.quantity + 1
-    //     }
-    //     return newItem;
-    //   }
-    //   return item;
-    // })
-
-    // setCart(updatedCart);
+    let updatedProducts = [...products].map( item => {
+      if(item._id == updatedProduct._id){
+        return {
+          ...updatedProduct,
+          quantity: item.quantity + updatedProduct.quantity
+        };
+      } else {
+        return item;
+      }
+    })
+    setCart(updatedCart);
+    setProducts(updatedProducts);
   }
 
   return (
@@ -98,8 +132,8 @@ export default function Store({store}) {
           return (
             <div key={i}>
               <h2>{product.name}</h2>
-              <h2>{product.price}</h2>
-            {/*<Product product={product}/>*/}
+              <h2>{product.quantity}</h2>
+              <button onClick={() => removeItemsFromCart(product._id)}>remove</button>
             </div>
           );
         })}
