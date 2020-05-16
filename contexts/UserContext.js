cb50974c4d0eea97cebb8faca7d29c8b733b5be7
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-16 15:53:06 -0300

    add new contexts for store name, products and cart

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -1,3 +1,23 @@
 import { createContext } from 'react';
 
 export const UserContext = createContext(null);
+export const ProductsContext = createContext(null);
+export const CartContext = createContext(null);
+export const StoreContext = createContext(null);
+
+// import { ProductsContext, CartContext, StoreContext } from '../../../contexts/UserContext';
+// import Loading from '../../../components/Loading/Loading';
+
+// const [isLoading, setIsLoading] = useState(true);
+//   const { products, setProducts } = useContext(ProductsContext);
+//   const { cart, setCart } = useContext(CartContext);
+//   const { storeName, setStoreName } = useContext(StoreContext);
+
+//   useEffect(() => {
+//     if (products === null || store.name !== storeName) {
+//       setStoreName(store.name);
+//       setProducts(store.products);
+//     }
+//     setIsLoading(false);
+//     // window.localStorage.setItem('products', JSON.stringify(products))
+//   }, [products, cart]);
