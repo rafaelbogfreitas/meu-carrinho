8aa770885cdc0eb446d92cc2ade2edca68861b50
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-18 11:17:05 -0300

    Add metatags handler to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,19 @@ import Head from 'next/head'
 import Login from '../components/Login/Login'
 import Signup from '../components/Signup/Signup'
 
+import { renderMetatags } from '../services/helpers'
+
 export default function Home() {
 
   return (
     <>
     <Head>
-      <title>Minha Loja</title>
+      {renderMetatags(
+        'Minha Loja',
+        'Crie sua loja e administre suas vendas',
+        'http://localhost:3000',
+        'http://nossologo.svg',
+      )}
     </Head>
       {/* Navbar placeholder */}
 
