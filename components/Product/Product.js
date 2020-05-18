cc830bba946afd6039ccd89b5b1530688757add4
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-17 23:15:31 -0300

    use owner and client feature HOC on products

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import productStyles from './product.module.scss'
+import React from 'react';
+import productStyles from './product.module.scss';
+import OwnerFeature from '../OwnerFeature/OwnerFeature';
+import ClientFeature from '../ClientFeature/ClientFeature';
 
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Product = ({
-     name, 
-     description, 
-     price, 
-     imageUrl,
-     quantity,
-     handleProduct,
-     handleDelete,
-     storeName,
-     _id
-    }) => {
+  name,
+  description,
+  price,
+  imageUrl,
+  quantity,
+  handleProduct,
+  handleDelete,
+  storeName,
+  _id,
+}) => {
   return (
     <aside className="product">
-        <Link
-          href={'/store/[name]/product/[id]'}
-          as={`/store/${storeName}/product/${_id}`}
-          key={_id}
-        >
-          <a key={_id}>{name}</a>
-        </Link>
-        <img src={imageUrl} alt={name}/>
-        <div>{description}</div>
-        <div className="quantity">{quantity}</div>
-        <div className="price">{price},00 R$</div>
+      <Link
+        href={'/store/[name]/product/[id]'}
+        as={`/store/${storeName}/product/${_id}`}
+        key={_id}
+      >
+        <a key={_id}>{name}</a>
+      </Link>
+      <img src={imageUrl} alt={name} />
+      <div>{description}</div>
+      <div className="quantity">{quantity}</div>
+      <div className="price">{price},00 R$</div>
+      <ClientFeature>
         <button onClick={() => handleProduct(_id)}>add</button>
+      </ClientFeature>
+      <OwnerFeature>
         <button onClick={() => handleDelete(_id)}>DELETE</button>
+      </OwnerFeature>
     </aside>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
