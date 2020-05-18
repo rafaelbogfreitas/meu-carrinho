9ecec80d3626ac0c3b5568089ae2fdb790329058
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-18 09:28:23 -0300

    Add fragment to the expression

--- services/helpers.js
+++ services/helpers.js
@@ -27,21 +27,23 @@ export const handleFileChange = (e, handler) => handler(e.target.files[0]);
 //generate metatags 
 export const renderMetatags = (title, description, url, imageUrl) => {
   return (
-    <meta name="title" content={title}/>
-    <meta name="description" content={description}/>
+    <>
+      <meta name="title" content={title}/>
+      <meta name="description" content={description}/>
 
 
-    <meta property="og:type" content="website"/>
-    <meta property="og:url" content={url}/>
-    <meta property="og:title" content={title}/>
-    <meta property="og:description" content={description}/>
-    <meta property="og:image" content={imageUrl}/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content={url}/>
+      <meta property="og:title" content={title}/>
+      <meta property="og:description" content={description}/>
+      <meta property="og:image" content={imageUrl}/>
 
 
-    <meta property="twitter:card" content="summary_large_image"/>
-    <meta property="twitter:url" content={url}/>
-    <meta property="twitter:title" content={title}/>
-    <meta property="twitter:description" content={description}/>
-    <meta property="twitter:image" content={imageUrl}/>
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content={url}/>
+      <meta property="twitter:title" content={title}/>
+      <meta property="twitter:description" content={description}/>
+      <meta property="twitter:image" content={imageUrl}/>
+    </>
   )
 }
