90e9e9af4175e44d1dacd661cde9b32d1bbaea44
Daniel Paiva <danielcspaiva@gmail.com>
2020-05-21 23:55:33 -0300

    updated landing page styles

--- pages/index.js
+++ pages/index.js
@@ -29,22 +29,21 @@ export default function Home() {
       </nav>
 
       <section className="main-section">
-        <div className="container container--row">
-          <article className="main-section__info">
-            <h1>Meu Carrinho</h1>
-            <h2>O carrinho virtual para a sua loja</h2>
-            <p>
-              98% dos consumidores estão nas redes sociais, crie seu carrinho
-              virtual e chegue até eles mais facilmente
-            </p>
-          </article>
+        <div className="container">
           <figure className="main-section__figure">
             <img
               className="main-section__figure__img"
-              src="/image2.svg"
-              alt="prancheta"
+              src="/meucarrinho_logo_azul.png"
+              alt="logo"
             ></img>
           </figure>
+          <article className="main-section__info">
+            <h2>O carrinho virtual para a sua loja</h2>
+            <p>
+              Mais de 90% estão nas redes sociais, crie seu carrinho
+              virtual e chegue até eles mais facilmente
+            </p>
+          </article>
         </div>
       </section>
 
@@ -54,7 +53,7 @@ export default function Home() {
 
           <section className="container--row">
             <article className="instructions-section__info">
-              <h3>Crie sua loja</h3>
+              <h3>1. Crie sua loja</h3>
               <p>
                 Crie sua loja virtual personalizada e adicione os seus produtos.
               </p>
@@ -74,7 +73,7 @@ export default function Home() {
         <div className="container container--home">
           <section className="container--row-reverse">
             <article className="instructions-section__info">
-              <h3>Divulgue nas redes sociais</h3>
+              <h3>2. Divulgue nas redes sociais</h3>
               <p>
                 Encontre os seus clientes aonde eles estiverem, divulgue sua
                 loja nas redes sociais mais utilizadas.
@@ -95,7 +94,7 @@ export default function Home() {
         <div className="container container--home">
           <section className="container--row">
             <article className="instructions-section__info">
-              <h3>Receba pedidos dos clientes</h3>
+              <h3>3. Receba pedidos dos clientes</h3>
               <p>
                 Receba pedidos facilmente através do whatsapp e notificação via
                 email.
@@ -116,7 +115,7 @@ export default function Home() {
         <div className="container container--home">
           <section className="container--row-reverse">
             <article className="instructions-section__info">
-              <h3>Acompanhe os pedidos</h3>
+              <h3>4. Acompanhe os pedidos</h3>
               <p>
                 Acompanhe o status dos pedidos no seu painel de vendas e
                 monitore o desempenho da sua loja através de gráficos.
